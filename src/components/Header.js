bc43cc4c4b58196c56692e5031bd4e3b4d2fafcb
Bob F <bobfdev@gmail.com>
2022-05-29 15:17:37 -0500

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Header.css';
+import { 
+    IoMdMenu,
+    IoMdImage,
+    IoMdCall,
+    IoMdClipboard,
+    IoMdHammer,
+    IoMdChatboxes,
+    IoMdHome,
+    IoMdPerson,
+    IoMdBookmark,
+    IoMdClose
+} from 'react-icons/io';
+
+
+const Header = () => {
+    const [active, setActive] = useState(false);
+
+    const activateNav = () => {
+        setActive(!active);
+    }
+
+  return (
+    <div className={active ? 'header' : 'header-mobile'}>
+        <div className="menu-icon" onClick={activateNav}>
+            {!active ? <IoMdMenu className='menu' /> : <IoMdClose className='menu' />}
+        </div>
+
+        <nav>
+            <ul className={active ? 'ul-item' : 'ul-item oicon'}>
+                <li>
+                    <IoMdImage className='icon' />
+                    <Link to='/'>Alumni</Link>
+                </li>
+                <li>
+                    <IoMdBookmark className='icon' />
+                    <Link to='/'>History</Link>
+                </li>
+                <li>
+                    <IoMdPerson className='icon' />
+                    <Link to='/'>Testimonials</Link>
+                </li>
+                <li>
+                    <IoMdHome className='icon' />
+                    <Link to='/'>Partners</Link>
+                </li>
+                <li>
+                    <IoMdChatboxes className='icon' />
+                    <Link to='/'>About</Link>
+                </li>
+                <li>
+                    <IoMdHammer className='icon' />
+                    <Link to='/'>Tutorials</Link>
+                </li>
+                <li>
+                    <IoMdCall className='icon' />
+                    <Link to='/'>Contact</Link>
+                </li>
+                <li>
+                    <IoMdClipboard className='icon' />
+                    <Link to='/'>FAQ</Link>
+                </li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
